c3eb68bac7bcd02198c2bb55f26bd4e5ec0b48ea
Jawadqureshi0900 <jawad.qureshi@numble.be>
2022-11-21 10:16:18 +0100

    var changed to let and indentation fixed

--- Background-color-selector/script.js
+++ Background-color-selector/script.js
@@ -1,19 +1,17 @@
-var h3 = document.querySelector("h3");
-var color1 = document.querySelector(".color-one");
-var color2 = document.querySelector(".color-two");
-var body = document.getElementById("gradient");
+let h3 = document.querySelector("h3");
+let color1 = document.querySelector(".color-one");
+let color2 = document.querySelector(".color-two");
+let body = document.getElementById("gradient");
 
 function setGradientColor () {
     body.style.background =
-        "linear-gradient(to right, "
-        + color1.value
-        + ", "
-        + color2.value
-        + ")";
-        
-        h3.textContent = body.style.background + ";";
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+    h3.textContent = body.style.background + ";";
 }
-
 
 color1.addEventListener("input", setGradientColor);
 color2.addEventListener("input", setGradientColor);
